0ca3217db0529d99bd2db8a1543fdafa4183016f
Balogh Dénes <balogh.denes@yahoo.com>
2019-05-05 12:06:17 +0300

    First commit

---
+++ Lab1/Models/MoviesDbSeeder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Models
+{
+    public class MoviesDbSeeder
+    {
+        public static void Initialize(MoviesDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any movies.
+            if (context.Movies.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Movies.AddRange(
+                new Movie
+                {
+                    Title = "1984",
+                    Runtime = 2,
+                    Rating = 7.5,
+                    Storyline = "Nice movie"
+                },
+                new Movie
+                {
+                    Title = "The Godfather",
+                    Runtime = 3,
+                    Rating = 9.5,
+                    Storyline = "It's a long story..."
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
